0effac332b531bb8622d47e4e59251f6fec242ff
KodamaNoMori <neon314@protonmail.com>
2025-10-23 12:23:47 +1100

    Plus one alg fixed

--- plus_one.py
+++ plus_one.py
@@ -3,27 +3,18 @@ from typing import List
 import pytest
 
 
-
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         if not digits:
             return [0]
 
-        last_index = len(digits) - 1
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
 
-        def one_over_increment(index, count_nine = 0):
-            if digits[index] == 9:
-                digits[index] = 0
-                count_nine += 1
-                one_over_increment(index - 1, count_nine)
-            else:
-                if (index == -1) & (count_nine > 0):
-                    digits = [0] + digits
-                digits[index+1] = digits[index+1] + 1
-
-        one_over_increment(last_index)
-
-        return digits
+        return [1] + digits
 
 def test_example_1():
     plus_one = Solution().plusOne
@@ -55,31 +46,13 @@ def test_example_3():
 def test_example_4():
     plus_one = Solution().plusOne
 
-    digits = None
-    expected_output = [0]
-
-    assert expected_output == plus_one(digits=digits)
-
-
-def test_example_5():
-    plus_one = Solution().plusOne
-
-    digits = [0, 0, 0]
-    expected_output = [0, 0, 1]
-
-    assert expected_output == plus_one(digits=digits)
-
-
-def test_example_5():
-    plus_one = Solution().plusOne
-
     digits = [9, 9, 9, 9]
-    expected_output = [1, 0, 0, 0]
+    expected_output = [1, 0, 0, 0, 0]
 
     assert expected_output == plus_one(digits=digits)
 
 
-def test_example_6():
+def test_example_5():
     plus_one = Solution().plusOne
 
     digits = [1, 9]
@@ -88,7 +61,7 @@ def test_example_6():
     assert expected_output == plus_one(digits=digits)
 
 
-def test_example_7():
+def test_example_6():
     plus_one = Solution().plusOne
 
     digits = [1, 9, 3]
@@ -96,7 +69,7 @@ def test_example_7():
 
     assert expected_output == plus_one(digits=digits)
 
-def test_example_8():
+def test_example_7():
     plus_one = Solution().plusOne
 
     digits = [0]
