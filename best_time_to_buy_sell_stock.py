f35bf724c593cf6e3c84e4f427340b8891020a24
KodamaNoMori <neon314@protonmail.com>
2025-10-23 12:17:32 +1100

    Examples

--- best_time_to_buy_sell_stock.py
+++ best_time_to_buy_sell_stock.py
@@ -3,12 +3,13 @@ from typing import List, Optional
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
         _min = None
         _min_index = None
         _max = None
         _max_index = None
 
-        for i in range(0, len(prices)):
+        for i in range(1, len(prices)):
             price = prices[i]
 
             if (_min is None) or (price < _min):
--- remove_duplicated_from_sorted_array.py
+++ remove_duplicated_from_sorted_array.py
@@ -1,64 +1,55 @@
 import pytest
 from typing import List
-import pytest
-from typing import List
 
 
 class Solution:
-    def __init__(self):
-        pass
+    def removeDuplicates(self, nums: List[int]) -> int:
+        write_pos = 1
 
-    def removeDuplicates(self, nums: List[int]) -> (int, List[int]):
-        if not nums:
-            return 0, nums
+        for read_pos in range(1, len(nums)):
+            if nums[read_pos] != nums[read_pos - 1]:
+                nums[write_pos] = nums[read_pos]
+                write_pos += 1
 
-        write_index = 1
-
-        for read_index in range(1, len(nums)):
-            if nums[read_index] != nums[read_index - 1]:
-                nums[write_index] = nums[read_index]
-                write_index += 1
-
-        for i in range(write_index, len(nums)):
-            nums[i] = None
-
-        return write_index, nums
+        return write_pos
 
 
-@pytest.fixture
-def remove_dup_obj():
-    return Solution().removeDuplicates
+def test_input_list_len_eq_output_list_len_first():
+    remove_dups = Solution().removeDuplicates
 
-def test_input_list_len_eq_output_list_len_first(remove_dup_obj):
-    input_list = None
-    k = 20
-    expected_output = None
-    k_result, output_list = remove_dup_obj(nums=input_list)
-    assert len(input_list) == expected_output
-    assert k_result == k
-
-def test_input_list_len_eq_output_list_len_first(remove_dup_obj):
     input_list = [1, 1, 2]
     k = 2
-    expected_output = [1, 2, None]
-    k_result, output_list = remove_dup_obj(nums=input_list)
-    assert len(input_list) == len(output_list)
-    assert k_result == 2
-    assert output_list == expected_output
 
-def test_input_list_len_eq_output_list_len_second(remove_dup_obj):
+    assert k == remove_dups(nums=input_list)
+
+def test_input_list_len_eq_output_list_len_second():
+    remove_dups = Solution().removeDuplicates
+
     input_list = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
     k = 5
-    expected_output = [0, 1, 2, 3, 4, None, None, None, None, None]
-    k_result, output_list = remove_dup_obj(nums=input_list)
-    assert len(input_list) == len(output_list)
-    assert k_result == k
 
-def test_unique_elements_in_output_list(remove_dup_obj):
-    pass
+    assert k == remove_dups(nums=input_list)
 
-def test_element_between_min_and_max(remove_dup_obj):
-    pass
+def test_all_duplicates():
+    remove_dups = Solution().removeDuplicates
 
-def test_non_decreasing_order_in_output_list(remove_dup_obj):
-    pass
+    input_list = [0, 0, 0, 0]
+    k = 1
+
+    assert k == remove_dups(nums=input_list)
+
+def test_len_one_list():
+    remove_dups = Solution().removeDuplicates
+
+    input_list = [10]
+    k = 1
+
+    assert k == remove_dups(nums=input_list)
+
+def test_no_duplicates():
+    remove_dups = Solution().removeDuplicates
+
+    input_list = [1,2,3,4]
+    k = 4
+
+    assert k == remove_dups(input_list)
